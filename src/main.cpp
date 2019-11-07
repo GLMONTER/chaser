1a8d75297776974924a58b4ed53dca0c29cc755c
Tad Keller <logischgl@gmail.com>
2019-11-06 17:58:39 -0800

    upgrade kernel to 4.20, upgrade lvgl to 5.3

--- src/main.cpp
+++ src/main.cpp
@@ -16,15 +16,17 @@
 pros::vision_signature_s_t sig = pros::Vision::signature_from_utility(1, 273, 2061, 1167, -5189, -4777, -4983, 5.200, 0);
 
 
+pros::vision_signature_s_t sig2 = pros::Vision::signature_from_utility(1, 8369, 8775, 8572, -1729, -1391, -1560, 11.000, 0);
+
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 pros::Motor frontRight(1, pros::E_MOTOR_GEARSET_18, true);
 pros::Motor frontLeft(11, pros::E_MOTOR_GEARSET_18, false);
 
-pros::Motor backRight(9, pros::E_MOTOR_GEARSET_18, true);
-pros::Motor backLeft(20, pros::E_MOTOR_GEARSET_18, false);
+pros::Motor backRight(10, pros::E_MOTOR_GEARSET_18, true);
+pros::Motor backLeft(9, pros::E_MOTOR_GEARSET_18, false);
 
-pros::Vision vs(10, pros::E_VISION_ZERO_CENTER);
+pros::Vision vs(1, pros::E_VISION_ZERO_CENTER);
 
 
 pros::ADIDigitalOut sens(3, true);
@@ -39,28 +41,30 @@ void opcontrol()
 	backLeft.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
 
 	vs.clear_led();
-	vs.set_signature(1, &sig);
 
-	extern const lv_img_t ball;
+	vs.set_signature(1, &sig2);
+	LV_IMG_DECLARE(ball);
+	//extern const lv_img_t ball;
 	lv_obj_t * im = lv_img_create(lv_scr_act(), NULL);
+
 	lv_img_set_src(im, &ball);
 	lv_obj_set_pos(im, 0, 0);
 
 
  lv_obj_t * xoffL =  lv_label_create(lv_scr_act(), NULL);
- lv_obj_align(xoffL, NULL, LV_ALIGN_CENTER, 125, -75);
+ lv_obj_align(xoffL, NULL, LV_ALIGN_CENTER, 100, -75);
 
  lv_obj_t * wid =  lv_label_create(lv_scr_act(), NULL);
- lv_obj_align(wid, NULL, LV_ALIGN_CENTER, 125, -50);
+ lv_obj_align(wid, NULL, LV_ALIGN_CENTER, 100, -50);
 
  lv_obj_t * state =  lv_label_create(lv_scr_act(), NULL);
- lv_obj_align(state, NULL, LV_ALIGN_CENTER, 125, -25);
+ lv_obj_align(state, NULL, LV_ALIGN_CENTER, 100, -25);
 
  lv_obj_t * name =  lv_label_create(lv_scr_act(), NULL);
- lv_obj_align(name, NULL, LV_ALIGN_CENTER, 125, 0);
+ lv_obj_align(name, NULL, LV_ALIGN_CENTER, 100, 0);
 
  lv_obj_t * name2 =  lv_label_create(lv_scr_act(), NULL);
- lv_obj_align(name2, NULL, LV_ALIGN_CENTER, 125, 20);
+ lv_obj_align(name2, NULL, LV_ALIGN_CENTER, 100, 20);
 
  lv_label_set_text(name, "Programmed by");
  lv_label_set_text(name2, "Tad Keller");
@@ -70,7 +74,7 @@ void opcontrol()
 		pros::vision_object_s_t val = vs.get_by_sig(0, 1);
 
 		std::string xoff = "X Offset : " + std::to_string(val.x_middle_coord);
-		std::string widV = "Ball Width : " + std::to_string(val.width);
+		std::string widV = "Cube Width : " + std::to_string(val.width);
 
 		lv_label_set_text(xoffL, xoff.c_str());
 		lv_label_set_text(wid, widV.c_str());
@@ -104,9 +108,9 @@ void opcontrol()
 		}
 		else
 		{
-			lv_obj_set_pos(im, 30, val.x_middle_coord * -1);
+			lv_obj_set_pos(im, 30, val.x_middle_coord * 1);
 
-		if(val.width < 50 && val.width != 0)
+		if(val.width < 100 && val.width != 0)
 		{
 			lv_label_set_text(state, "forward");
 			backLeft.move(90);
@@ -119,7 +123,7 @@ void opcontrol()
 			sens.set_value(true);
 		}
 	}
-	if((!(val.width < 50 && val.width != 0) && !(val.x_middle_coord > 70 || val.x_middle_coord < -70)) || (val.x_middle_coord < -300 || val.x_middle_coord > 300))
+	if((!(val.width < 100 && val.width != 0) && !(val.x_middle_coord > 70 || val.x_middle_coord < -70)) || (val.x_middle_coord < -300 || val.x_middle_coord > 300))
 	{
 			lv_label_set_text(state, "idle");
 
